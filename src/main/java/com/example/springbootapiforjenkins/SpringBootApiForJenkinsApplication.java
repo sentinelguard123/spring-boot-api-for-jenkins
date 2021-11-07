dcf9f99d85cba509fd2af7b25703c5d98c0bd70a
Camilo del Real <cjdelreal2009@gmail.com>
2021-11-07 09:45:42 +0100

    ADD: Base project

---
+++ src/main/java/com/example/springbootapiforjenkins/SpringBootApiForJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootapiforjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApiForJenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApiForJenkinsApplication.class, args);
+    }
+
+}
